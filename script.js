7496577b18f49ec4476c5010fadf803ce09b7e08
Anton Böhler <antboehler@gmail.com>
2024-05-28 21:35:20 +0200

    improve mobile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const CLICK_MOVEMENT_THRESHOLD = 5;
 
 function navigationDragSlideStart(e) {
     const element = e.target;
+    e.preventDefault();
     element.setPointerCapture(e.pointerId);
 
     const width = element.getBoundingClientRect().width;
@@ -12,6 +13,7 @@ function navigationDragSlideStart(e) {
     let isClick = true;
 
     const pointermove = e => {
+        e.preventDefault();
         let movement = e.offsetX - startX;
         if(Math.abs(movement) > CLICK_MOVEMENT_THRESHOLD) {
             isClick = false;
@@ -21,6 +23,7 @@ function navigationDragSlideStart(e) {
     };
 
     const pointerup = e => {
+        e.preventDefault();
         if(isClick) {
             let percentage = Math.min(Math.max(e.offsetX / width, 0), 100);
             element.style.setProperty("--percentage", percentage);
@@ -42,12 +45,14 @@ function navigationCustomSliderStart(e) {
     const width = element.getBoundingClientRect().width;
 
     const pointermove = e => {
+        e.preventDefault();
         let percentage = Math.min(Math.max(e.offsetX / width, 0), 100);
         element.style.setProperty("--percentage", percentage);
     };
     pointermove(e);
 
     const pointerup = e => {
+        e.preventDefault();
         element.removeEventListener("pointermove", pointermove);
         element.removeEventListener("pointerup", pointerup);
         element.releasePointerCapture(e.pointerId);
